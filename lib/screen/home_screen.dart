d358be689f61cbdadfae5bfd7e5b93c021a407ed
twentiecker <twentiecker@gmail.com>
2023-07-14 10:54:45 +0700

    set dynamic size_2

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -5,9 +5,11 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:provider/provider.dart';
+import 'package:story_app_flutter/utils/color_theme.dart';
 
 import '../provider/home_provider.dart';
 import '../provider/upload_provider.dart';
+import '../utils/style_theme.dart';
 import 'camera_screen.dart';
 
 class HomePage extends StatefulWidget {
@@ -29,117 +31,253 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
+    final Size screenSize = MediaQuery.of(context).size;
+    final ratio = screenSize.height / 803.137269;
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Camera Project"),
-        actions: [
-          IconButton(
-            onPressed: () {
-              if (formKey.currentState!.validate()) {
-                _onUpload(descController.text);
-              }
-            },
-
-            /// todo-04-after-01: update the widget
-            icon: context.watch<UploadProvider>().isUploading
-                ? const CircularProgressIndicator(
-                    color: Colors.white,
-                  )
-                : const Icon(Icons.upload),
-            tooltip: "Unggah",
-          ),
-        ],
-      ),
+      backgroundColor: grey,
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Expanded(
-              flex: 3,
-              child: context.watch<HomeProvider>().imagePath == null
-                  ? const Align(
-                      alignment: Alignment.center,
-                      child: Icon(
-                        Icons.image,
-                        size: 100,
-                      ),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              SizedBox(height: screenSize.height * 0.04),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        InkWell(
+                          onTap: () {
+                            Navigator.pop(context);
+                          },
+                          child: Icon(
+                            Icons.arrow_back_ios_new,
+                            color: white,
+                            size: ratio * 24,
+                          ),
+                        ),
+                        SizedBox(width: screenSize.width * 0.04),
+                        Text(
+                          'Create Stories',
+                          style: headlineSmall(
+                            context,
+                            ratio,
+                          ),
+                        ),
+                      ],
+                    ),
+                    InkWell(
+                      onTap: () {
+                        if (formKey.currentState!.validate()) {
+                          _onUpload(descController.text, ratio);
+                        }
+                      },
+                      child: context.watch<UploadProvider>().isUploading
+                          ? const CircularProgressIndicator(
+                              color: Colors.white,
+                            )
+                          : Row(
+                              children: [
+                                Text(
+                                  'Post',
+                                  style: titleSmall(
+                                    context,
+                                    ratio,
+                                  ),
+                                ),
+                                SizedBox(width: screenSize.width * 0.01),
+                                Icon(
+                                  Icons.check_rounded,
+                                  color: white,
+                                  size: ratio * 20,
+                                ),
+                              ],
+                            ),
                     )
-                  : _showImage(),
-            ),
-            Form(
-              key: formKey,
-              child: TextFormField(
-                controller: descController,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter description.';
-                  }
-                  return null;
-                },
+                  ],
+                ),
               ),
-            ),
-            Expanded(
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  ElevatedButton(
-                    onPressed: () => _onGalleryView(),
-                    child: const Text("Gallery"),
-                  ),
-                  ElevatedButton(
-                    onPressed: () => _onCameraView(),
-                    child: const Text("Camera"),
-                  ),
-                  ElevatedButton(
-                    onPressed: () => _onCustomCameraView(),
-                    child: const Text("Custom Camera"),
-                  ),
-                ],
+              SizedBox(height: screenSize.height * 0.04),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                height: screenSize.height * 0.45,
+                child: context.watch<HomeProvider>().imagePath == null
+                    ? Align(
+                        alignment: Alignment.center,
+                        child: Icon(
+                          Icons.image,
+                          size: screenSize.height * 0.2,
+                          color: Colors.grey,
+                        ),
+                      )
+                    : _showImage(),
               ),
-            )
-          ],
+              SizedBox(height: screenSize.height * 0.03),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Form(
+                  key: formKey,
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Enter your description",
+                      hintStyle: titleMedium(
+                        context,
+                        ratio,
+                        Colors.grey,
+                        null,
+                      ),
+                      prefixIcon: Icon(
+                        Icons.notes_rounded,
+                        color: lightGreen,
+                        size: ratio * 24,
+                      ),
+                      fillColor: lightGrey,
+                      filled: true,
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(color: lightGrey),
+                        borderRadius: BorderRadius.circular(13),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(color: lightGreen),
+                        borderRadius: BorderRadius.circular(13),
+                      ),
+                    ),
+                    style: titleMedium(
+                      context,
+                      ratio,
+                      white,
+                      null,
+                    ),
+                    cursorColor: white,
+                    maxLines: 8,
+                    controller: descController,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please input the description.';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+              ),
+              SizedBox(height: screenSize.height * 0.02),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Container(
+                  padding: const EdgeInsets.symmetric(vertical: 5),
+                  decoration: BoxDecoration(
+                    color: lightGrey,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () => _onGalleryView(),
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: lightGrey,
+                          elevation: 0,
+                        ),
+                        child: const Icon(
+                          Icons.image_outlined,
+                          color: lightGreen,
+                          size: 35,
+                        ),
+                      ),
+                      ElevatedButton(
+                        onPressed: () => _onCameraView(),
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: lightGrey,
+                          elevation: 0,
+                        ),
+                        child: const Icon(
+                          Icons.camera_alt_outlined,
+                          color: lightGreen,
+                          size: 35,
+                        ),
+                      ),
+                      ElevatedButton(
+                        onPressed: () => _onCustomCameraView(),
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: lightGrey,
+                          elevation: 0,
+                        ),
+                        child: const Icon(
+                          Icons.camera_outlined,
+                          color: lightGreen,
+                          size: 35,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(height: screenSize.height * 0.03),
+            ],
+          ),
         ),
       ),
     );
   }
 
-  _onUpload(String desc) async {
+  _onUpload(String desc, double ratio) async {
     final ScaffoldMessengerState scaffoldMessengerState =
         ScaffoldMessenger.of(context);
     final uploadProvider = context.read<UploadProvider>();
 
-    /// todo-02-prepare-01: check image is ready or not
     final homeProvider = context.read<HomeProvider>();
     final imagePath = homeProvider.imagePath;
     final imageFile = homeProvider.imageFile;
-    if (imagePath == null || imageFile == null) return;
+    if (imagePath == null || imageFile == null) {
+      return scaffoldMessengerState.showSnackBar(
+        SnackBar(
+          behavior: SnackBarBehavior.floating,
+          backgroundColor: green,
+          content: Text(
+            'Please upload the image',
+            style: titleMedium(
+              context,
+              ratio,
+              white,
+              null,
+            ),
+          ),
+        ),
+      );
+    }
 
-    /// todo-02-prepare-02: prepare a document variabel to upload
     final fileName = imageFile.name;
     final bytes = await imageFile.readAsBytes();
 
-    /// todo-05-compress-03: compress an image before upload
     final newBytes = await uploadProvider.compressImage(bytes);
-
-    /// todo-03-upload-07: upload the document
-    /// todo-05-compress-04: change the variable byte
     await uploadProvider.upload(
       newBytes,
-      // bytes,
       fileName,
       desc,
     );
 
-    /// todo-04-after-02: remove the image
     if (uploadProvider.uploadResponse != null) {
       homeProvider.setImageFile(null);
       homeProvider.setImagePath(null);
     }
 
-    /// todo-04-after-03: show the message
     scaffoldMessengerState.showSnackBar(
-      SnackBar(content: Text(uploadProvider.message)),
+      SnackBar(
+        behavior: SnackBarBehavior.floating,
+        backgroundColor: green,
+        content: Text(
+          uploadProvider.message,
+          style: titleMedium(
+            context,
+            ratio,
+            white,
+            null,
+          ),
+        ),
+      ),
     );
   }
 
